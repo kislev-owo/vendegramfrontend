e06e6f73fb2d3d853c3e8a22dc7b377659c52157
franfossi <franfossi@gmail.com>
2020-10-15 17:40:02 +0000

    se reposiciona la etiqueta de tiendas en la página Etiquetas

--- src/js/views/Etiquetas.js
+++ src/js/views/Etiquetas.js
@@ -25,18 +25,6 @@ export const Etiquetas = props => {
 				<div className="jumbotron ml-2 mr-2 mt-2 pt-4 ">
 					<div className="text-center mb-2">
 						<h2 className="text-center ml-4 mb-2 m-auto">
-							<div>
-								<button
-									className="btn btn-primary"
-									style={{ background: "#03989E" }}
-									onClick={() => {
-										actions.fetchCargarTiendas();
-										console.log("Esta es la vista de tiendas en TiendasCard");
-										history.push("../TiendasCard");
-									}}>
-									Tiendas
-								</button>
-							</div>
 							<span
 								className="badge badge-pill badge-info border mt-3 mb-2"
 								style={{ background: "#03989E" }}>
@@ -46,9 +34,21 @@ export const Etiquetas = props => {
 
 						<div className="text-center mt-2 mb-2">
 							{/* <div className="card-deck d-flex align-items-center"> */}
-							<div className="row row-cols-1 row-cols-md-4 mt-3 mb-5">
+							<div className="row row-cols-1 row-cols-md-4 mt-3 mb-3">
 								<EtiquetasCard />
 							</div>
+						</div>
+						<div>
+							<button
+								className="btn btn-primary"
+								style={{ background: "#03989E" }}
+								onClick={() => {
+									actions.fetchCargarTiendas();
+									console.log("Esta es la vista de tiendas en TiendasCard");
+									history.push("../TiendasCard");
+								}}>
+								Tiendas
+							</button>
 						</div>
 					</div>
 				</div>
